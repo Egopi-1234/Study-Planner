7a0fe80bf9a8144b5d45489b94a0efb262d5d8ec
Egopi-1234 <gopie852@gmail.com>
2025-07-26 14:26:05 +0530

    changes

---
+++ app/src/main/java/com/saveetha/studyplanner/api/UpdateStatusResponse.java
@@ -0,0 +1,41 @@
+package com.saveetha.studyplanner.api;
+
+import com.google.gson.annotations.SerializedName;
+import java.util.List;
+
+public class UpdateStatusResponse {
+    @SerializedName("status")
+    public boolean status;
+
+    @SerializedName("message")
+    public String message;
+
+    @SerializedName("data")
+    public List<TaskData> data;
+
+    public static class TaskData {
+        @SerializedName("id")
+        public int id;
+
+        @SerializedName("name")
+        public String name;
+
+        @SerializedName("description")
+        public String description;
+
+        @SerializedName("Date")
+        public String date;
+
+        @SerializedName("time")
+        public String time;
+
+        @SerializedName("Priority")
+        public String priority;
+
+        @SerializedName("user_Id")
+        public int userId;
+
+        @SerializedName("status")
+        public String status;
+    }
+}
